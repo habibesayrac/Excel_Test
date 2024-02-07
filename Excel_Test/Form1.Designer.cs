e470fdf308faaddcf0e3166b70ff12042bb3db65
habibesayrac <habibesayrac@outlook.com>
2024-02-07 09:48:25 +0300

    Add project files.

---
+++ Excel_Test/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Excel_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnListele = new System.Windows.Forms.Button();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSaat = new System.Windows.Forms.TextBox();
+            this.TxtDersAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(593, 405);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.Location = new System.Drawing.Point(622, 27);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(321, 43);
+            this.BtnListele.TabIndex = 1;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = true;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(622, 92);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(321, 44);
+            this.BtnKaydet.TabIndex = 2;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(679, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Saat:";
+            // 
+            // TxtSaat
+            // 
+            this.TxtSaat.Location = new System.Drawing.Point(732, 154);
+            this.TxtSaat.Name = "TxtSaat";
+            this.TxtSaat.Size = new System.Drawing.Size(206, 32);
+            this.TxtSaat.TabIndex = 4;
+            // 
+            // TxtDersAdi
+            // 
+            this.TxtDersAdi.Location = new System.Drawing.Point(732, 192);
+            this.TxtDersAdi.Name = "TxtDersAdi";
+            this.TxtDersAdi.Size = new System.Drawing.Size(206, 32);
+            this.TxtDersAdi.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ders Adı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 437);
+            this.Controls.Add(this.TxtDersAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtSaat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 11F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BtnListele;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSaat;
+        private System.Windows.Forms.TextBox TxtDersAdi;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Excel_Test/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Net.Configuration;
+
+namespace Excel_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection connection = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\habib\OneDrive\Desktop\1.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;'");
+
+        void listele()
+        {
+            connection.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter("Select * from [Sheet1$]", connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            connection.Close();
+        }
+
+
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+
+            listele();
+
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            OleDbCommand command = new OleDbCommand("insert into [Sheet1$] (Saat,Ders) values (@p1,@p2)", connection);
+            command.Parameters.AddWithValue("@p1", TxtSaat.Text);
+            command.Parameters.AddWithValue("@p2", TxtDersAdi.Text);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Ekleme başarılı!");
+            connection.Close();
+            listele();
+
+        }
+
+    }
+}
